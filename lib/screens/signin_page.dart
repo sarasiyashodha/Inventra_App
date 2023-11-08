ec3c764672c95bcbe21d96e6d33f16390dc1d5cf
Sarasi Yashodha <sarasiyashodha67@gmail.com>
2023-11-08 11:12:11 +0530

    Edited background image of signin page

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -42,80 +42,102 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(22.0),
 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 47.0,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 450,
+            left: 0,
+            right: -90,
+            child: Transform.rotate(
+              angle: 3.14,
+              child: Image.asset(
+                'images/background.png',
+                width: 377.0,
+                height: 378.0,
               ),
-              Image(image: AssetImage('images/name.png'), width: 205.0, height: 52.0, alignment: Alignment.topLeft),
-              Text('Sign in to your account',
-                style: ksubTitleTextStyle,
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    MyTextField(
-                      controller: usernameController,
-                      hintText: 'Username',
-                      obscureText: false,
-                      icon: Icons.person,
-                      onChanged: (val) {
-                      },
-                    ),
-                    const SizedBox(height: 44.0),
-                    MyTextField(
-                      controller: passwordController,
-                      hintText: 'Password',
-                      obscureText: true,
-                      icon: Icons.lock,
-                      onChanged: (val) {
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(22.0),
 
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 22.0,),
-              Text('Forgot password?',
-                style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w700, color: Color(0XFF1AC9CD), fontSize: 16.0),),
-              SizedBox(height: 30.0,),
-              GestureDetector(
-                onTap: () => _login(context),
-
-                child: Container(
-                  padding: const EdgeInsets.all(25.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xff18C9CD),
-                    borderRadius: BorderRadius.circular(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 47.0,
                   ),
-                  child: Center(
-                    child: Text(
-                      "Sign In",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Lato',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
+                  Image(image: AssetImage('images/name.png'), width: 205.0, height: 52.0, alignment: Alignment.topLeft),
+                  Text('Sign in to your account',
+                    style: ksubTitleTextStyle,
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        MyTextField(
+                          controller: usernameController,
+                          hintText: 'Username',
+                          obscureText: false,
+                          icon: Icons.person,
+                          onChanged: (val) {
+                          },
+                        ),
+                        const SizedBox(height: 44.0),
+                        MyTextField(
+                          controller: passwordController,
+                          hintText: 'Password',
+                          obscureText: true,
+                          icon: Icons.lock,
+                          onChanged: (val) {
+
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 22.0,),
+                  Text('Forgot password?',
+                    style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w700, color: Color(0XFF1AC9CD), fontSize: 16.0),),
+                  SizedBox(height: 30.0,),
+                  GestureDetector(
+                    onTap: () => _login(context),
+
+                    child: Container(
+                      padding: const EdgeInsets.all(25.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xff18C9CD),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Lato',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-             SizedBox(
-               height: 275.0,
-             ),
-             BottomLine(),
-            ],
-          ),
 
+
+                ],
+              ),
+
+            ),
+          ),
+          Positioned(
+            top: 765,
+              left: 112,
+              child: BottomLine()),
+        ],
       ),
     );
   }
